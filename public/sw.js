d9f417e70dff7ceeff0af872be95cf276cb19cff
Clark Kitchen <goldbar123467@users.noreply.github.com>
2026-07-18 14:16:16 -0400

    Advance school release cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "parkworks-v14-9de2d43fb6-school-sandbox-delay";
+const CACHE_VERSION = "parkworks-v15-9de2d43fb6-school-sandbox-delay";
 const SHELL_URLS = ["/manifest.webmanifest", "/parkworks-icon.png"];
 
 async function installShell() {
